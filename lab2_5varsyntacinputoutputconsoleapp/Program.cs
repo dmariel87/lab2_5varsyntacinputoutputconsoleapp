579623e98c118732347153b257c4dd46ab656aa2
dmariel87@gmail.com <dmariel87@gmail.com>
2020-06-16 13:26:53 -0400

    lab2_5 version1

---
+++ lab2_5varsyntacinputoutputconsoleapp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab2_5varsyntacinputoutputconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Lab 2_5
+
+            // 1. Write a C# program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user.
+            int intFirstNumber = 0;
+            int intSecondNumber = 0;
+            int intResult = 0;
+            dbl dblResult = 0;
+
+
+
+
+            // 2. Write a C# program to print Hello and your name (which you will type in on the Console) in a separate line.
+
+            // 3. Write a C# program to print the output of multiplication of three numbers which will be entered by the user.
+
+            // 4. Write a C# program that takes an age (for example 20) as input and prints something like "You lo
+            // 4. Write a C# program that takes an age (for example 20) as input and prints something like "You look younger than 20 (the age the user entered)".
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
